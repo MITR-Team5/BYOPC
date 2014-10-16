899089689b10f1203604ff279f067b909f4f6ba8
alibi <ali.bischak@gmail.com>
2014-10-16 19:04:05 -0400

    Create portalhome.php

---
+++ portalhome.php
@@ -0,0 +1,30 @@
+<?php include 'header.php'; ?>
+
+	<img src="bdlogo.png" height="90px" width="250px"> 
+    	
+    <div id="topbar"></div>
+    <div id="middlebar"></div>
+    <div id="bottombar"></div>
+
+    <div id="content">
+    	<h1> Welcome to BD's BYO-PC Information Hub </h1>
+
+	<div id="userlogin">
+
+	  <form name="loginInput">
+	    <p>Username: <input type="text" name="username" id="username" size="30" maxlength="40" class= "inputfield" required /></p>
+
+	    <p>Password: <input type="password" name="pwd" id="pwd" size="30" maxlength="40" class= "inputfield" required /></p>
+
+	  </form>
+
+	</div>
+
+
+	<input id = "loginButton" type="button" value="Log In" /> 
+
+
+<?php include 'footer.php'; ?>
+
+
+	
