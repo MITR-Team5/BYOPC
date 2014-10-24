142afe7e32b63024da15e88231d3f1bfd18a58e1
mikeogod <mikeogod@gmail.com>
2014-10-23 22:51:01 -0400

    home is index

--- index.php
+++ index.php
@@ -25,6 +25,31 @@ if(count($ERRORS)==0)
 ?>
 <!doctype html>
 <html>
-  <?php include "header.php"; ?>
-  <?php include "body.php"; ?>
+  	<?php include 'header.php'; ?>
+
+	<img src="bdlogo.png" height="90px" width="250px"> 
+    	
+    <div id="topbar"></div>
+    <div id="middlebar"></div>
+    <div id="bottombar"></div>
+
+    <div id="content">
+    	<h1> Welcome to BD's BYO-PC Information Hub </h1>
+
+	<div id="userlogin">
+
+	  <form name="loginInput">
+	    <p>Username: <input type="text" name="username" id="username" size="30" maxlength="40" class= "inputfield" required /></p>
+
+	    <p>Password: <input type="password" name="password" id="pwd" size="30" maxlength="40" class= "inputfield" required /></p>
+
+	  </form>
+
+	</div>
+
+
+	<input id = "loginButton" type="button" value="Log In" /> 
+
+
+	<?php include 'footer.php'; ?>
 </html>
