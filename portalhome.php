1aaff5defa9df13cf87caaa3f1b37102720e41a0
alibi <ali.bischak@gmail.com>
2014-10-29 00:00:34 -0400

    portalhome = index

--- portalhome.php
+++ portalhome.php
@@ -1,4 +1,5 @@
 <?php include 'header.php'; ?>
+<?php include 'session.php'; ?>
 
 	<img src="bdlogo.png" height="90px" width="250px"> 
     	
@@ -14,7 +15,7 @@
 	  <form name="loginInput">
 	    <p>Username: <input type="text" name="username" id="username" size="30" maxlength="40" class= "inputfield" required /></p>
 
-	    <p>Password: <input type="password" name="pwd" id="pwd" size="30" maxlength="40" class= "inputfield" required /></p>
+	    <p>Password: <input type="password" name="password" id="pwd" size="30" maxlength="40" class= "inputfield" required /></p>
 
 	  </form>
 
