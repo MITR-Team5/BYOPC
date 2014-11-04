95b055d9be1763ff0e4d785629e4f562a43e07f7
mikeogod <mikeogod@gmail.com>
2014-11-04 15:26:19 -0500

    mike

--- portal.js
+++ portal.js
@@ -5,43 +5,17 @@ $(document).ready(
       $("#loginButton").click(login);
       // $("#logoutButton").click(logout);
 	  
-      //$("#userRegister").hide();
-      $("#loginButton").click(showlogin);
-      // $("#logoutButton").click(logout);
-      $("#userloginButton").click(sendlogin);
-      //$("#registerButton").click(showregister);
-      $("#userRegisterButton").click(sendreg);
 
 
 });
 
 function login() {
- //  	if($("#target").text() == "User not logged in!"){
-	//     $("#userlogin").show();
-	//     $("#loginButton").css("margin-top","30%");
-	//     $("#footer").css("margin-top", "10%");
-	//     $("#username").focus();
-	// }
-	// else{
 	$("#loginButton").hide();
 	$("#userlogin").show();
 	$("#footer").css("margin-top", "36%");
-	//}
-    //$("#userlogin").hide();
-    //$("#userRegister").hide();
-    //$("#loginButton").click(showlogin);
-    // $("#logoutButton").click(logout);
-    //$("#userloginButton").click(sendlogin);
+    $("#userloginButton").click(sendlogin);
     //$("#registerButton").click(showregister);
-    //$("#userRegisterButton").click(sendreg);
-
-}
-
-function showlogin() {
-	$("#userlogin").show();
-	$("#loginButton").hide();
-	$("#username").focus();
-	$("#footer").css("margin-top", "36%");
+    $("#userRegisterButton").click(sendreg);
 
 }
 
