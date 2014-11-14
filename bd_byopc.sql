e908a225899214ab5039bcc8e07ce43cf419ff83
mikeogod <mikeogod@gmail.com>
2014-11-13 19:37:52 -0500

    admin page

--- bd_byopc.sql
+++ bd_byopc.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2014 at 06:26 PM
+-- Generation Time: Nov 14, 2014 at 01:37 AM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
@@ -55,10 +55,10 @@ CREATE TABLE IF NOT EXISTS `survey_questions` (
 --
 
 INSERT INTO `survey_questions` (`id`, `desc`, `created_at`, `type`) VALUES
-(1, 'desc0', '2014-11-04 18:04:26', 'YesNo'),
-(2, 'desc1', '2014-11-04 18:04:26', 'Numeric'),
-(3, 'desc2', '2014-11-04 18:04:26', 'YesNo'),
-(4, 'desc3', '2014-11-08 00:46:30', 'YesNo');
+(1, 'Would you like to pay for your work laptop out of your own pocket?', '2014-11-04 18:04:26', 'YesNo'),
+(2, 'I am satisfied with my current BD issued laptop', '2014-11-04 18:04:26', 'Numeric'),
+(3, 'I currently own a personal laptop', '2014-11-04 18:04:26', 'YesNo'),
+(4, 'I would like to bring my personal laptop into the office', '2014-11-08 00:46:30', 'YesNo');
 
 -- --------------------------------------------------------
 
@@ -75,7 +75,7 @@ CREATE TABLE IF NOT EXISTS `survey_results` (
   PRIMARY KEY (`id`),
   KEY `userid` (`userid`),
   KEY `surveyid` (`surveyid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=39 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=47 ;
 
 --
 -- Dumping data for table `survey_results`
@@ -85,7 +85,15 @@ INSERT INTO `survey_results` (`id`, `userid`, `surveyid`, `value`, `created_at`)
 (35, 4, 1, 1, '2014-11-08 17:09:46'),
 (36, 4, 4, 1, '2014-11-08 17:09:55'),
 (37, 4, 2, 5, '2014-11-08 17:09:55'),
-(38, 4, 3, 0, '2014-11-08 17:09:55');
+(38, 4, 3, 0, '2014-11-08 17:09:55'),
+(39, 5, 1, 1, '2014-11-13 21:08:06'),
+(40, 5, 2, 1, '2014-11-13 21:08:20'),
+(41, 5, 3, 1, '2014-11-13 21:08:20'),
+(42, 5, 4, 1, '2014-11-13 21:08:20'),
+(43, 6, 1, 1, '2014-11-13 21:08:56'),
+(44, 6, 4, 1, '2014-11-13 21:09:02'),
+(45, 6, 2, 2, '2014-11-13 21:09:02'),
+(46, 6, 3, 1, '2014-11-13 21:09:02');
 
 -- --------------------------------------------------------
 
@@ -100,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `salt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `users`
@@ -108,7 +116,9 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `username`, `role`, `password`, `salt`) VALUES
 (3, 'mike', 'admin', '9c01c96cd422112c19494de74ef52bab3caab6b2fb9d9ebe5b3a66faf313d9f1', '3c1935ca170b6614'),
-(4, 'joker', 'normal', 'c8b939ea3d14bae4b5181da2b2b57f8b542e3cb639882ba83ffeba41f27599d1', '1c71b27a1dee24bd');
+(4, 'joker', 'normal', 'c8b939ea3d14bae4b5181da2b2b57f8b542e3cb639882ba83ffeba41f27599d1', '1c71b27a1dee24bd'),
+(5, 'joe', 'normal', '122024dbde90dacfd02736b1fda1a2e64eb4b34edc30acc3c2b703b51ff87068', '3aca586d37051418'),
+(6, 'Kat', 'normal', '17cc96c9835e4016ddba42b3497709460b47584538d2e69b996746af0421e2ed', '9c8d91eb72ea3b');
 
 --
 -- Constraints for dumped tables
