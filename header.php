46304fbe9b2afcea92aa55ed3db4d0ade10a54b9
mikeogod <mikeogod@gmail.com>
2014-11-15 22:39:01 -0500

    reorganized the javscript, css, and changed controller.php to service.php

--- header.php
+++ header.php
@@ -2,10 +2,10 @@
 	<!--<script src="pong.js"> </script>
 	<script src="jquery-2.0.3.min.js"></script>-->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script src="portal.js"></script>
+	<script src="javascript/portal.js"></script>
 	<title>BYO-PC @ BD
 	</title>
-	<link rel="stylesheet" type="text/css" href="portal.css">
+	<link rel="stylesheet" type="text/css" href="css/portal.css">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
 </head>
 
